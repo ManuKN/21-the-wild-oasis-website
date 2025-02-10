36c11f448efa85fe6ba2b614eedd048d5fca9a26
Manu K N <201910101325@presidencyuniversity.in>
2025-02-10 21:57:10 +0530

    finished section 33

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,7 @@ function RootLayout({ children }) {
       <body className={`bg-primary-950 ${josefin.className} relative text-primary-100 min-h-screen flex flex-col`}>
         <Header />
         <div className='flex-1 px-8 py-12 grid'>
-          <main className='max-w-7xl mx-auto'>{children}</main>
+          <main className='max-w-7xl mx-auto w-full'>{children}</main>
         </div>
       </body>
     </html>
