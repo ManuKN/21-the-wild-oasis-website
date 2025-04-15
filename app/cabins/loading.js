9b13075d9fc9717a8311e9a65ad5eb4c471b6846
Manu <manu.kn@myn.team>
2025-04-15 10:42:35 +0530

    changes added till the 458

--- app/cabins/loading.js
+++ app/cabins/loading.js
@@ -1,9 +1,10 @@
-import Spinner from "./_components/Spinner";
+import Spinner from "../_components/Spinner";
+
 
 
 export default function Loading() {
     return <div className="grid justify-center items-center">
         <Spinner />
-        <p className="text-xl text-primary-200">Loading...</p>
+        <p className="text-xl text-primary-200">Loading cabins data...</p>
     </div>
 }
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,12 +1,15 @@
+import { Suspense } from "react";
 import CabinCard from "../_components/CabinCard";
+import CabinList from "../_components/CabinsList";
+import { getCabins } from "../_lib/data-service";
+import Spinner from "../_components/Spinner";
+import Loading from "./loading";
 
 export const metadata = {
   title: 'Cabins',
 };
 
-export default function Page() {
-  // CHANGE
-  const cabins = [];
+export default async function Page() {
 
   return (
     <div>
@@ -17,18 +20,14 @@ export default function Page() {
         Cozy yet luxurious cabins, located right in the heart of the Italian
         Dolomites. Imagine waking up to beautiful mountain views, spending your
         days exploring the dark forests around, or just relaxing in your private
-        hot tub under the stars. Enjoy nature's beauty in your own little home
+        hot tub under the stars. Enjoy nature&#39;s beauty in your own little home
         away from home. The perfect spot for a peaceful, calm vacation. Welcome
         to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Loading />}>
+        <CabinList />
+      </Suspense >
     </div>
   );
 }
